c8b3c9bb7e183fe3300c0b6fd7b731a2591f0d2e
Maximilian Güntner <code@mguentner.de>
2021-01-05 19:40:08 +0100

    use podman (fedora)

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ CIDFILE=$(mktemp /tmp/build-container.XXXXXXXXX)
 IIDFILE=$(mktemp /tmp/image-id.XXXXXXXXX)
 [[ -e $CIDFILE ]] && rm $CIDFILE
 
-docker build --iidfile $IIDFILE .
-docker run --cidfile $CIDFILE  -e PACKAGE=$1 $(cat $IIDFILE) /usr/bin/build-package
-docker cp $(cat $CIDFILE):/tmp/all-packages.tar .
+podman build --iidfile $IIDFILE .
+podman run --cidfile $CIDFILE  -e PACKAGE=$1 $(cat $IIDFILE) /usr/bin/build-package
+podman cp $(cat $CIDFILE):/tmp/all-packages.tar .
 [[ -e $CIDFILE ]] && rm $CIDFILE
-#[[ -e $IIDFILE ]] && rm $IIDFILE
+[[ -e $IIDFILE ]] && rm $IIDFILE
